2ccd484dd01b84f97f194506b7e81de3b229ec75
haohaoyang <haohaoyang@astri.org>
2023-02-23 14:31:48 +0800

    filelist version1

--- filelist.f
+++ filelist.f
@@ -1,16 +1,16 @@
 ./top_tb.sv
-./my_case0.sv
+#./my_case0.sv
 #./my_case1.sv
 ./my_if.sv
-./my_transaction.sv
+#./my_transaction.sv
 ./my_driver.sv
-./my_monitor.sv
-./my_agent.sv
-./my_model.sv
-./my_env.sv
-./my_scoreboard.sv
-./my_sequencer.sv
-./base_test.sv
+#./my_monitor.sv
+#./my_agent.sv
+#./my_model.sv
+#./my_env.sv
+#./my_scoreboard.sv
+#./my_sequencer.sv
+#./base_test.sv
 
 #../../sim1/top.v
 
