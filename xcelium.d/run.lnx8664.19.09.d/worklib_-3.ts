d21c923d35c8a074821971502e378ac7d0c193f3
haohaoyang <haohaoyang@astri.org>
2023-03-13 09:06:26 +0800

    orignial version2

--- xcelium.d/run.lnx8664.19.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1526655669 /edatools/cadence/XCELIUM-19.09.010/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1526655664 /edatools/cadence/XCELIUM-19.09.010/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
