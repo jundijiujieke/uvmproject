ada8ec9ac8570b31ec53d663ce6853d0d1f4a017
haohaoyang <haohaoyang@astri.org>
2023-05-09 09:25:23 +0800

    second time

--- component/testing.sh
+++
@@ -1,7 +0,0 @@
-for ((n=0;n<2;n++))
-do
-make SIZE1=128 SEED1=`bash -c 'echo $RANDOM'` -s
-make SIZE1=1024 SEED1=`bash -c 'echo $RANDOM'` -s
-done
-
-grep TEST xrun_mode*.log
--- mode/uvm.cmd
+++
@@ -1,4 +0,0 @@
-database -open -shm uvm
-probe -create -database uvm -shm -memories -all -depth all top_tb
-run
-
